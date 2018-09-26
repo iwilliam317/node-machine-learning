2d83d89f61b67e704dd409614ce96f184e07a3ed
william <willshinji@gmail.com>
2018-09-26 19:52:11 -0300

    provide more entries to neural trainning

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const brain = require('brain.js');
 
 const network = new brain.NeuralNetwork();
 
-network.train([{input: { age: 22 }, output: { adult: 1 }},
-           {input: { age: 5 }, output: { kid: 1 }},
-           {input: { age: 16 }, output: { teenage: 1 }}]);
+network.train([
+  { input: { age: 22 }, output: { adult: 1 } },
+  { input: { age: 18 }, output: { adult: 1 } },
+  { input: { age: 20 }, output: { adult: 1 } },
+  { input: { age: 27 }, output: { adult: 1 } },
+  { input: { age: 21 }, output: { adult: 1 } },
+  { input: { age: 5 }, output: { kid: 1 } },
+  { input: { age: 7 }, output: { kid: 1 } },
+  { input: { age: 16 }, output: { teenage: 1 } },
+  { input: { age: 15 }, output: { teenage: 1 } },
+  { input: { age: 13 }, output: { teenage: 1 } },
+  ]);
 
 const result = network.run({ age: 22}); 
 
