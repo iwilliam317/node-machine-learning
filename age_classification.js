77b0ca196ab8e8110d70fce56c63a7b8ca799fef
william <willshinji@gmail.com>
2018-09-27 20:22:10 -0300

    format output log

--- age_classification.js
+++ age_classification.js
@@ -8,13 +8,17 @@ network.train([
   { input: { age: 20 }, output: { adult: 1 } },
   { input: { age: 27 }, output: { adult: 1 } },
   { input: { age: 21 }, output: { adult: 1 } },
-  { input: { age: 5 }, output: { kid: 1 } },
-  { input: { age: 7 }, output: { kid: 1 } },
+  { input: { age: 5 }, output: { child: 1 } },
+  { input: { age: 7 }, output: { child: 1 } },
   { input: { age: 16 }, output: { teenage: 1 } },
   { input: { age: 15 }, output: { teenage: 1 } },
   { input: { age: 13 }, output: { teenage: 1 } },
   ]);
 
-const result = network.run({ age: 22}); 
+const adultProbability = network.run({ age: 22}); 
+const childProbability = network.run({ age: 4}); 
 
-console.log(result);
+console.group('Probabilities - Based on entries provided!');
+console.log(`Age: 22 => ${adultProbability.adult}`);
+console.log(`Age: 4 => ${childProbability.child}`);
+console.groupEnd();
